eaf41db50309055d97e66d73fd105a3bf2ffb6a2
Eugen Wintersberger <eugen.wintersberger@desy.de>
2017-06-01 11:25:18 +0200

    Changed the Boost version and simplified the build configuration

--- conanfile.py
+++ conanfile.py
@@ -4,14 +4,13 @@ import os
 
 class PnicoreConan(ConanFile):
     name = "pnicore"
-    requires = (("Boost/1.64.0@wintersb/stable","private"),)
     version = "1.1.0"
     license = "GPL V2"
     url = "<Package recipe repository url here, for issues about the package>"
     settings = "os", "compiler", "build_type", "arch"
     options = {"shared": [True, False],
-               "with_conan_boost":[True,False]}
-    default_options = "shared=True","with_conan_boost=False"
+               "with_system_boost":[True,False]}
+    default_options = "shared=True","with_system_boost=False"
     generators = "cmake"
     build_policy="missing"
     description = """
@@ -20,17 +19,27 @@ class PnicoreConan(ConanFile):
     tree. The master brachn is considered to always build.
     """
 
+    boost_package = "Boost/1.62.0@lasote/stable"
+
+    def configure(self):
+        #setting up boost if required
+        if not self.options.with_system_boost:
+            self.requires(self.boost_package)
+            
+            self.options["Boost"].shared = self.options.shared
+
     def source(self):
         self.run("git clone https://github.com/pni-libraries/libpnicore.git")
         # This small hack might be useful to guarantee proper /MT /MD linkage in MSVC
         # if the packaged project doesn't have variables to set it properly
-#        tools.replace_in_file("libpnicore/CMakeLists.txt", "include(CTest)",
-#'''
-#include(CTest)
-#include(${CMAKE_BINARY_DIR}/conanbuildinfo.cmake)
-#conan_basic_setup()
-#'''
-#)
+        tools.replace_in_file("libpnicore/CMakeLists.txt", "include(CTest)",
+'''
+include(CTest)
+include(${CMAKE_BINARY_DIR}/conanbuildinfo.cmake)
+conan_basic_setup()
+'''
+        )
+
 
     def build(self):
         cmake = CMake(self)
@@ -39,24 +48,14 @@ class PnicoreConan(ConanFile):
         if self.options.shared:
             cmake_defs["BUILD_SHARED_LIBS"]="ON"
 
-        if self.options.with_conan_boost:
-            self.output.info("PNICORE will be built with conan provided Boost")
-            cmake_defs["PNICORE_CONAN_BOOST"] = "ON"
-        else:
-            self.output.info("PNICORE will be built with system boost libraries")
 
         cmake_defs["CMAKE_INSTALL_PREFIX"] = self.package_folder
 
         cmake.configure(source_dir="libpnicore",
                         defs=cmake_defs)
-        
-        cmake.build()
 
-        if self.settings.os=="Windows":
-            cmake.build(target="RUN_TESTS")
-        else:
-            cmake.build(target="test")
-
+        cmake.build(target="all")
+        cmake.build(target="test")
         cmake.build(target="install")
 
 
