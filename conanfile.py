1f135eb1e7446664725b7b0319ca3cb865c58a5b
Eugen Wintersberger <eugen.wintersberger@desy.de>
2017-11-30 11:52:15 +0100

    Looks good - we have a reasonable build
    The code behaves now as it should.

--- conanfile.py
+++ conanfile.py
@@ -1,5 +1,6 @@
 from conans import ConanFile, CMake, tools
 import os
+import git
 
 
 class PnicoreConan(ConanFile):
@@ -34,25 +35,49 @@ class PnicoreConan(ConanFile):
     """
 
     boost_package = "Boost/1.62.0@lasote/stable"
+    pnicore_git_url = "https://github.com/pni-libraries/libpnicore.git"
+    
+    def _get_local_current_commit(self,repository_path):
+        self.output.info("Trying to access repository in: "+repository_path)
+        commit = None
+        try:
+            self.run("cd %s && git pull" %repository_path) 
+            repo = git.Repo(repository_path)
+            commit = repo.commit().hexsha
+            self.output.info("Current commit is: "+commit)
+            
+        except:
+            self.output.info("Could not retrieve current commit of sources")
+            
+        return commit
+    
+    def _get_remote_current_commit(self):
+        self.output.info("Trying to get latest commit from remote repository")
+        gcmd = git.cmd.Git()
+        commit = None
+        
+        try:
+            commit = gcmd.ls_remote(self.pnicore_git_url,"refs/heads/master").split("\t")[0]
+            self.output.info("The current remote master is on: %s" %commit)
+        except:
+            self.output("Failure to determine the current commit from remote")
+            
+        return commit
     
     def _get_current_commit(self):
         #we pull here the repository and add the commit to the build options. 
         #if the commit has changed the hash of the build configuration will change
         #and thus force a rebuild of the package
         
-        import git
+        
         current_commit = None
         self.output.info("Checking the GIT commit")       
         source_path =  os.path.join(self.conanfile_directory,"..","source","libpnicore")
-        self.output.info("Trying to access repository in: "+source_path)
-        try:
-            self.run("cd %s && git pull" %source_path) 
-            repo = git.Repo(source_path)
-            current_commit = repo.commit().hexsha
-            self.output.info("Current commit is: "+current_commit)
-            
-        except:
-            self.output.info("Could not retrieve current commit of sources")
+        
+        if os.path.exists(source_path):
+            current_commit = self._get_local_current_commit(source_path)
+        else:
+            current_commit = self._get_remote_current_commit()
             
         return current_commit
     
@@ -62,9 +87,8 @@ class PnicoreConan(ConanFile):
             #if we can obtain the actual commit of the repository we can do something with it
             self.options.commit = current_commit
         
-        
-
     def configure(self):
+        self.output.info("Setting the configuration")
         #setting up boost if required
         if not self.options.with_system_boost:
             self.requires(self.boost_package)
@@ -75,7 +99,8 @@ class PnicoreConan(ConanFile):
         
 
     def source(self):
-        self.run("git clone https://github.com/pni-libraries/libpnicore.git")
+        self.output.info("Cloning git repository")
+        self.run("git clone %s" %self.pnicore_git_url)
         self.run("cd libpnicore && git submodule init && git submodule update --remote")
         # This small hack might be useful to guarantee proper /MT /MD linkage in MSVC
         # if the packaged project doesn't have variables to set it properly
@@ -91,6 +116,7 @@ conan_basic_setup()
 
 
     def build(self):
+        self.output.info("Running the build")
         cmake = CMake(self)
 
         if self.options.shared:
